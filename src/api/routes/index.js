ad92c4df82f36f4af09ed00177e7ef1d0eecec0a
Jitul Teron <jitulteron9@gmail.com>
2021-10-31 15:16:43 +0530

    testing route again

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 router.use("/users", require("./user"));
 router.use("/group", require("./group"));
-router.use("/testing", (req,res,next)=>{
+router.get("/testing", (req,res,next)=>{
     res.status(200).send("API WORKING !!");
 });
 module.exports = router;
